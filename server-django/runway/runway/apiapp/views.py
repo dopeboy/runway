929545b4f40706dbe652428dad0436b561805d91
Manish Sinha <arithmetic@gmail.com>
2015-05-28 23:14:14 -0400

    refresh

---
+++ server-django/runway/runway/apiapp/views.py
@@ -0,0 +1,40 @@
+from apiapp.models import Photo, MyUser
+from apiapp.serializers import PhotoSerializer, UserSerializer
+from rest_framework import permissions
+from apiapp.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import viewsets
+
+
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'photos': reverse('photo-list', request=request, format=format)
+    })
+
+
+class PhotoViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+
+    Additionally we also provide an extra `highlight` action.
+    """
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = MyUser.objects.all()
+    serializer_class = UserSerializer
